f2c98d45e453dfce66da0bcec12c85f9044336ec
Hellen Avila Rosa <hellenar12@gmail.com>
2018-07-04 19:12:53 -0300

    TUDO CERTO UHUL

--- genio_MIPS.asm
+++ genio_MIPS.asm
@@ -10,9 +10,13 @@ ativacao: 		.asciiz "\n Digite o número de ativação:  "
 velocidade: 		.asciiz "\n Digite o número de velocidade:  " 
 score: 			.asciiz "\n ACERTOU"
 loser: 			.asciiz "\n ERROU"
+.align 2
+ringbuffer: .space 32
 
 .text
 main:
+	la $a0, ringbuffer 
+	jal init
 	menu_jogo:
 	#imprimir MENU
 		li $v0, 4			#string
@@ -23,12 +27,12 @@ main:
 		syscall				#int 
 	
 	bne $v0, 1, sai_encerra
-		
 		#imprime mensagem de ativacao
 		li $v0, 4			#string
 		la $a0, ativacao		#mensagem
 		syscall
 		#512/16 -  pixel maior - 32 espacos 
+		
 		jal desenha_jogo
 		la $a0, vetor				#vetor random
 		#le entrada - numero
@@ -201,14 +205,17 @@ addi $sp, $sp, -24
 sw  $a0, 0($sp)			#vetor
 sw  $ra, 4($sp)			#retorno
 	
-	move $t3, $a0
-	
-	lui	 $a0, 0xFFFF			#carrega endereço
-	get_char:
-	lw	 $t0, 0($a0)			#controle
-	andi $t0, $t0, 0x00000001	#esperar
-	beq  $t0, $zero, get_char	#enquanto nao houver comunicacao 
-	lw   $v0, 4($a0)			#salva os dados
+	#move $t3, $a0		#VETOR
+	#CHAR
+	li $t0,2				# pra habilitar interrupcao
+	sw $t0,0xffff0000			#habilitar teclado
+	#checar ring buffer vazio ou nao
+	empty_ring_loop:
+		la $a0, ringbuffer
+		jal rbuf_empty
+		bnez $v0, empty_ring_loop
+			sw $zero, 0xffff0000
+			jal read
 	
 	lw $t1, 0($sp)
 	if_0:
@@ -218,7 +225,7 @@ sw  $ra, 4($sp)			#retorno
 			jal acende
 			#imprimir score
 			li $v0, 4				#string
-			la $a0, score			#le mensagem
+			la $a0, score				#le mensagem
 			syscall
 			li $t2, 1				#flag
 			j sai_compara			
@@ -589,7 +596,7 @@ escreve_l:
 	return_l:
 	jr $ra
 coluna:
-	li   $t0, 0x10010000	#t3 = endereco base
+	li   $t0, 0x10010000		#t3 = endereco base
 	sll  $t1, $a0, 5		#y = y * 512/16  -  vertical -2^5=16 -> 16+16 -> 16x16 bloco de 32
 	addu $t1, $a1, $t1		# adds x, y to the first pixel (t0)- horizontal
 	sll  $t1, $t1, 2		#1 pixel - 4 bytes	
@@ -598,10 +605,128 @@ coluna:
 escreve_c:
 	beq $t2, $a2, return_c
 		#escreve
-		sw   $a3, 0($t0)    #put the color branc ($a2) in $t0
+		sw   $a3, 0($t0)    	#put the color branc ($a2) in $t0
 		addi $t0, $t0, 128	#anda pelo vetor 4*tl -> tl = 32 
 		addi $t2, $t2, 1	#int posicao do pixel
 	j escreve_c
 	return_c:
 	jr $ra
-########################################################################
+###################################################################################
+#RING BUFFER
+init:
+	sw $zero, 0($a0)		#rbuf[size]
+	sw $zero, 4($a0)		#rbuf[wr]
+	sw $zero, 8($a0)		#rbuf[rd]
+	jr $ra
+
+read:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+
+ 	li $t0, 0      			#tmp = 0
+ 	jal rbuf_empty     		#rbuf_empty(rbuf)
+  	move $t1, $v0      		#t1 = v0
+ 	bnez  $t1, rbuf_empty_c   	#continua - verdadeiro(1) se tiver vazio
+ 		move $v0, $t0    
+    		lw   $t2, 0($a0)      	#t2 = size
+    		subi $t2, $t2, 1    	#t2 = size--
+    		sw   $t2, 0($a0)
+    		
+    		#tmp = rbuf->buf[rbuf->rd]
+    		lw   $t1, 8($a0)      	#acesso - rbuf->rd
+    		addi $t3, $a0, 12    	#endereco do buf - &rbuf->buf[0]
+    		add  $t4, $t3, $t1    	#&rbuf->buf[rbuf->rd]
+    		lbu  $t5, 0($t4)    	#rbuf->buf[rbuf->rd] - CHAR
+    		add  $t0, $zero, $t5    #tmp = rbuf->buf[rbuf->rd]
+    		#rbuf->rd = (rbuf->rd + 1) % MAX_SIZE
+    		addi $t1, $t1, 1    	#rbuf->rd + 1
+    		li   $t6, 16      	#MAX_SIZE
+    		remu $t4, $t3, $t6    	#(rbuf->rd + 1) % MAX_SIZE
+    		sw   $t4, 8($a0)      	#rbuf->rd = (rbuf->rd + 1) % MAX_SIZE
+    		move $v0, $t0      	#v0 = tmp
+   	rbuf_empty_c:
+   	lw $ra, 0($sp)
+   	addi $sp, $sp, 8
+    	jr $ra
+    	
+rbuf_empty:
+	lw   $t0, 0($a0)		#t0 = size
+	beqz $t0, if_empty		#rbuf->size == 0
+		li  $v0, 0		#return 0
+		jr  $ra
+	if_empty:
+	li $v0, 1			#return 1
+	jr $ra
+
+rbuf_full:
+	lw $t0, 0($a0)
+	beq $t0, 16, if_full	#rbuf->size == MAX_SIZE
+	li, $v0, 0				#return 0
+	jr $ra
+	
+	if_full:
+	li $v0, 1				#return 1
+	jr $ra
+	
+write_ring:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	
+  	move $t0, $a0    #t0 = *rbuf
+	move $t1, $a1    #t1 = byte
+  
+  	jal rbuf_full      #rbuf_empty(rbuf)
+  	move  $t2, $v0      #v0 = t2
+  	beqz  $t2, rbuf_full_c    #continua
+ 	li    $v0, 0      # return 0
+ 	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 8
+  	jr    $ra
+  
+  		rbuf_full_c:
+    		lw $t2, 0($a0)      #t2 = size
+    		addi $t2, $t2, 1    #t2 = size++
+    		sw $t2, 0($a0)      #t2 = size
+  
+  
+   		 #rbuf->buf[rbuf->wr] = byte
+    		lw  $t2, 4($a0)      #rbuf->wr
+   		add $t3, $a0, 12     #&rbuf->buf[0]
+		# sll $t2, $t2, 2      #rbuf->wr = t2 => t2*4 - CHAR
+    		add $t4, $t3, $t2    #&rbuf->buf[rbuf->wr]
+    		sb  $a1, 0($t4)      #rbuf->wr[rbuf->wr] = byte
+  
+   		 #rbuf->wr = (rbuf->wr + 1) % MAX_SIZE
+    		addi $t2, $t2, 1    #rbuf->wr + 1
+    		li   $t6, 16      #MAX_SIZE
+   		remu $t4, $t3, $t6    #(rbuf->wr + 1) % MAX_SIZE
+   		sw   $t4, 4($a0)    #rbuf->wr = (rbuf->wr + 1) % MAX_SIZE
+    		li $v0, 1      #return 1
+    		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+    		jr $ra
+
+############################################################################
+.ktext 0x80000180
+
+addi $sp, $sp, -8
+sw $ra, 0($sp)
+sw $s0, 4($sp)
+
+	lb $a1, 0xffff0004
+	la $a0, ringbuffer			# space 32
+	la $s0, write_ring
+	jalr $s0
+
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	addi $sp, $sp,8
+	li $t0, 2
+	sw $zero, 0xffff0004
+	sw $t0, 0xffff0000
+	li $t0, 0x0000ff11
+eret					#seta o coprocessador 0 para 0
+######################################################################################
+
+	
